ade94b0df800bfb5afcc5f0ea343ff09b8f77c5b
YunusEmreErturk <yemrerturk@gmail.com>
2019-12-19 16:43:35 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    currentCategory: "",
+    products: [],
+    cart: []
+  };
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  addToProductInCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(item => item.product.id === product.id);
+    if (addedItem) {
+      addedItem.quantity += 1;
+    } else {
+      newCart.push({ product: product, quantity: 1 });
+    }
+    this.setState({ cart: newCart });
+  };
+
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter(c => c.product.id !== product.id);
+    this.setState({ cart: newCart });
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }));
+  };
+
+  render() {
+    let productInfo = {
+      title: "Product List Title"
+    };
+
+    let categoryInfo = {
+      title: "Category List Title"
+    };
+    return (
+      <div className="App">
+        <Container>
+          <Navi removeFromCart ={this.removeFromCart} cart={this.state.cart}></Navi>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              ></CategoryList>
+            </Col>
+            <Col xs="9">
+              <ProductList
+                addToProductInCart={this.addToProductInCart}
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              ></ProductList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Navi.js
@@ -0,0 +1,56 @@
+import React, { useState,Component } from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+import CartSummary from './CartSummary';
+
+
+  export default class Navi extends Component {
+    constructor(props){
+      super(props);
+      this.toggle = this.toggle.bind(this);
+      this.state ={
+        isOpen:false
+      }
+
+    }
+    toggle(){
+      this.setState({
+        isOpen:!this.state.isOpen
+      })
+    }
+ 
+  render(){
+
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Devel App</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <CartSummary removeFromCart = {this.props.removeFromCart} cart={this.props.cart}/>
+            </Nav>
+            <NavbarText>{this.props.cart.length}
+            </NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+  
+}
